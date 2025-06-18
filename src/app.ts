6194aa640d5fe3abe37c9eb2200ff935666e4dd3
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-18 08:27:39 +0600

    setup filebase configuration

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.json({ message: "Hello from catalog service" });
 });
 
 app.use(globalErrorHandler);
