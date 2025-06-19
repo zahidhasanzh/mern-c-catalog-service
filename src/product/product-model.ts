89c099ba6a7057a527d1c1740347c4d37440b456
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-19 11:22:37 +0600

    add product mode and dummy data

---
+++ src/product/product-model.ts
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const attributeValueSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    value: {
+        type: mongoose.Schema.Types.Mixed,
+    },
+});
+
+const priceConfigurationSchema = new mongoose.Schema({
+    priceType: {
+        type: String,
+        enum: ["base", "additional"],
+    },
+    availableOptions: {
+        type: Map,
+        of: Number,
+    },
+});
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        priceConfiguration: {
+            type: Map,
+            of: priceConfigurationSchema,
+        },
+        attributes: [attributeValueSchema],
+        tenantId: {
+            type: String,
+            required: true,
+        },
+        categoryId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+        },
+        isPublish: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+export default mongoose.model("Category", productSchema);
