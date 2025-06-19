dc537089ca9d0c46ef6142912ddab27c67dadff0
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-06-19 08:33:57 +0600

    add authorization

---
+++ src/common/middlewares/canAccess.ts
@@ -0,0 +1,21 @@
+import { Response, Request, NextFunction } from "express";
+import { AuthRequest } from "../types";
+import createHttpError from "http-errors";
+
+export const canAccess = (roles: string[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const _req = req as AuthRequest;
+        const roleFromToken = _req.auth.role;
+
+        if (!roles.includes(roleFromToken)) {
+            const error = createHttpError(
+                403,
+                "You don't have enough permissions",
+            );
+            next(error);
+            return;
+        }
+
+        next();
+    };
+};
--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,3 +1,13 @@
+import { Request } from "express";
+
 export type AuthCookie = {
     accessToken: string;
 };
+
+export interface AuthRequest extends Request {
+    auth: {
+        sub: string;
+        role: string;
+        id?: string;
+    };
+}
