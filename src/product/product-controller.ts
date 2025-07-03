891783342b81d694437aa575461ca7687ee2a518
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-07-03 10:50:49 +0600

    remove extra db call for old image

--- src/product/product-controller.ts
+++ src/product/product-controller.ts
@@ -91,7 +91,7 @@ export class ProductController {
         let imageName: string | undefined;
         let oldImage: string | undefined;
         if (req.files?.image) {
-            oldImage = await this.productService.getProductImage(productId);
+            oldImage = product.image;
 
             const image = req.files.image as UploadedFile;
             imageName = uuidv4();
@@ -101,7 +101,7 @@ export class ProductController {
                 fileData: image.data,
             });
 
-            await this.storage.delete(oldImage!);
+            await this.storage.delete(oldImage);
         }
 
         const {
--- src/product/product-service.ts
+++ src/product/product-service.ts
@@ -6,11 +6,6 @@ export class ProductService {
         return await productModel.create(product);
     }
 
-    async getProductImage(productId: string) {
-        const product = await productModel.findById(productId);
-        return product?.image;
-    }
-
     async updateProduct(productId: string, product: Product) {
         return await productModel.findOneAndUpdate(
             { _id: productId },
