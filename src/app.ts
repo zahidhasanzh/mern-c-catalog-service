6b644572c1d5fd6d1b5080da9ce311b39a68ac52
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-07-05 13:36:28 +0600

    fix cors problem and this undefine issu

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express, { Request, Response } from "express";
+import config from "config";
+import cors from "cors";
+
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import categoryRouter from "./category/category-router";
 import productRouter from "./product/product-router";
+import toppingRouter from "./topping/topping-router";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
+const ALLOWED_DOMAINS = [config.get("frontend.adminUI")];
+app.use(
+    cors({
+        origin: ALLOWED_DOMAINS as string[],
+        credentials: true,
+    }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -15,6 +26,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/categories", categoryRouter);
 app.use("/products", productRouter);
+app.use("/toppings", toppingRouter);
 
 app.use(globalErrorHandler);
 
--- src/category/category-controller.ts
+++ src/category/category-controller.ts
@@ -12,6 +12,8 @@ export class CategoryController {
     ) {
         this.create = this.create.bind(this);
         this.update = this.update.bind(this);
+        this.index = this.index.bind(this);
+        this.getOne = this.getOne.bind(this);
     }
 
     async create(req: Request, res: Response, next: NextFunction) {
